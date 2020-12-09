4be75a3c42b7dff02d06db316a8b46e125ab8efa
Jena Boehm <boehmjena@gmail.com>
2020-12-08 22:21:04 -0800

    add lab 06 second model routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Tea = require('./lib/models/tea.js');
+const Houseplant = require('./lib/models/houseplant.js');
 const express = require('express');
 const app = express();
 
@@ -11,11 +12,16 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-
 app.get('/teas', (req, res) => {
   Tea
     .find()
     .then(teas => res.send(teas));
+});
+
+app.get('/houseplants', (req, res) => {
+  Houseplant
+    .find()
+    .then(houseplants => res.send(houseplants));
 });
 
 app.get('/teas/:id', (req, res) => {
@@ -24,11 +30,22 @@ app.get('/teas/:id', (req, res) => {
     .then(teas => res.send(teas));
 });
 
+app.get('/houseplants/:id', (req, res) => {
+  Houseplant
+    .findById(req.params.id)
+    .then(houseplants => res.send(houseplants));
+});
 
 app.post('/teas', async(req, res) => {
   Tea
     .insert(req.body)
     .then(teas => res.send(teas));
+});
+
+app.post('/houseplants', async(req, res) => {
+  Houseplant
+    .insert(req.body)
+    .then(houseplants => res.send(houseplants));
 });
 
 app.put('/teas/:id', (req, res) => {
@@ -37,10 +54,22 @@ app.put('/teas/:id', (req, res) => {
     .then(teas => res.send(teas));
 });
 
+app.put('/houseplants/:id', (req, res) => {
+  Houseplant
+    .update(req.params.id, req.body)
+    .then(houseplants => res.send(houseplants));
+});
+
 app.delete('/teas/:id', (req, res) => {
   Tea
     .delete(req.params.id)
     .then(teas => res.send(teas));
+});
+
+app.delete('/houseplants/:id', (req, res) => {
+  Houseplant
+    .delete(req.params.id)
+    .then(houseplants => res.send(houseplants));
 });
 
 module.exports = app;
